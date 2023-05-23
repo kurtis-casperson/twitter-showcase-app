053cf7dcaa4e72f86e2622581a283d46b3cd8e49
Kurtis <kcasperson7@gmail.com>
2023-05-22 22:13:28 -0700

    Update navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,15 @@ import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
-      <Container>
+    <Navbar
+      collapseOnSelect
+      //   fixed="top"
+      expand="sm"
+      bg="dark"
+      variant="dark"
+      className="navBar"
+    >
+      <Container className="navBarContainer">
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
