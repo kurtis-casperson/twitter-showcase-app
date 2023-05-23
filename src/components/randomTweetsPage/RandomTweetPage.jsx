481fbfe526b692d5d522ab81f3c8e9be3008934c
Kurtis <kcasperson7@gmail.com>
2023-05-22 22:13:49 -0700

    Random tweet component

--- src/components/randomTweetsPage/RandomTweetPage.jsx
+++ src/components/randomTweetsPage/RandomTweetPage.jsx
@@ -1,5 +1,29 @@
+import { Button } from 'react-bootstrap'
+
 const RandomTweetPage = () => {
-  return <h1 className="title">Popular Twitter Figures</h1>
+  const [randomTweet, setrandomTweet] = useState([])
+
+  //   const getRandomTweet = () => {
+  // const getTweet = Math.random(recent tweets)
+  // setrandomTweet(getTweet)
+  //   }
+
+  return (
+    <>
+      <h1 className="title">Popular Twitter Figures</h1>
+      <div>
+        <Button className="btn">
+          <img
+            id="button"
+            className="rounded-circle shadow-4-strong"
+
+            // src="./assets/martha.jpeg"
+          />
+        </Button>
+      </div>
+      <div id="random-tweet-display">{randomTweet}</div>
+    </>
+  )
 }
 
 export default RandomTweetPage
