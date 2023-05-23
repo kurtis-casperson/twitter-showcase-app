12a325f6230075a14071b36cf4f6017ab3b4aebe
Kurtis <kcasperson7@gmail.com>
2023-05-22 22:13:09 -0700

    Add searchPage component

--- src/components/searchPage/SearchPage.jsx
+++ src/components/searchPage/SearchPage.jsx
@@ -1,17 +1,65 @@
 import { Button } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
 
 const SearchPage = () => {
-  const handleInputSearch = () => {}
+  const [searchInput, setSearchInput] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  // const apiRequest = 'https://api.twitter.com/2/tweets/search/recent/?query='
 
+  const dummydata = {
+    data: [
+      {
+        edit_history_tweet_ids: ['1660756684367699969'],
+        id: '1660756684367699969',
+        text: 'RT @translatingTXT: 💛 kai!!!!!!!!! are you on weverse? i miss you\n\n🐧 yo hello~ \n\n@TXT_members @TXT_bighit \n#HUENINGKAI https://t.co/wrazURi…',
+      },
+      {
+        edit_history_tweet_ids: ['1660756682094391296'],
+        id: '1660756682094391296',
+        text: "@Breaking_Battle @tedcruz @SilverBulletLLC @SamThePol @MichelleUgenti Hello Ted, la réponse à la liberté doit égal à la lutte contre la criminalité au Texas, Nikki Haley, en n'a rapporté des faits dans une vidéo et moi des conclusions Ted…\nA bientôt Ted",
+      },
+      {
+        edit_history_tweet_ids: ['1660756681972936704'],
+        id: '1660756681972936704',
+        text: 'RT @shizukuorange: 20230520\n#TREASURE_HELLO_IN_HONGKONG\n#TREASURE #트레저 \n#요시 #ヨシ #YOSHI\n#박정우 #PARKJEONGWOO #パクジョンウ \n\n(*≧∀≦*)(*≧∀≦*) https://…',
+      },
+      {
+        edit_history_tweet_ids: ['1660756681855229955'],
+        id: '1660756681855229955',
+        text: 'RT @strawbicing: hello kitty pumps https://t.co/eILSOnOaz9',
+      },
+      {
+        edit_history_tweet_ids: ['1660756681393840130'],
+        id: '1660756681393840130',
+        text: 'RT @lovesickdoe: hello kitty cup https://t.co/oycxStxs6l',
+      },
+    ],
+  }
+
+  const handleInputSearch = (e) => {
+    setSearchInput(e.target.value)
+  }
+  const [tweetData, setTweetData] = useState(dummydata)
+
+  const tweetText = () => {
+    // setTweetData will be set to the request query with the searchInput
+    setTweetData(dummydata)
+    const searchResultArray = tweetData.data.map((each, index) => {
+      return <div key={index}>{each.text}</div>
+    })
+    setSearchResult(searchResultArray)
+  }
+
+  // when the button is pressed setSearchResult should contain the data from the dummydata object
+  // map over dummy data. if searchinput === data then return
   return (
     <>
       <div className="search-bar-container">
         <input
           id="searchBar"
           type="input"
-          placeholder="Search by name"
+          placeholder="Search any topic"
           name="searchBar"
-          value="enter text"
           onChange={handleInputSearch}
         ></input>
 
@@ -20,11 +68,14 @@ const SearchPage = () => {
           type="button"
           variant="info"
           size="sm"
-          onClick={() => {}}
+          onClick={() => {
+            tweetText(), setSearchInput('')
+          }}
         >
           Search Tweets
         </Button>
       </div>
+      <div id="grid">{searchResult}</div>
     </>
   )
 }
