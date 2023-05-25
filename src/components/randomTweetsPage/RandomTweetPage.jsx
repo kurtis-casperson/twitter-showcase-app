982fe3eb45c5e33ecb443ec38361cbd052fab84b
Kurtis <kcasperson7@gmail.com>
2023-05-24 22:38:32 -0700

    Add image styles

--- src/components/randomTweetsPage/RandomTweetPage.jsx
+++ src/components/randomTweetsPage/RandomTweetPage.jsx
@@ -1,5 +1,10 @@
 import { Button } from 'react-bootstrap'
-
+import { useState } from 'react'
+import martha from '../../assets/martha.jpg'
+import forbes from '../../assets/forbes.jpg'
+import ramsay from '../../assets/ramsay.jpg'
+import natgeo from '../../assets/natgeo.jpg'
+import './RandomTweetPage.css'
 const RandomTweetPage = () => {
   const [randomTweet, setrandomTweet] = useState([])
 
@@ -8,20 +13,50 @@ const RandomTweetPage = () => {
   // setrandomTweet(getTweet)
   //   }
 
+  // const [tweetData, setTweetData] = useState(randomTweet)
+
+  // const tweetText = () => {
+  //   // setTweetData will be set to the request query with the searchInput
+  //   setTweetData(dummydata)
+  //   const searchResultArray = tweetData.data.map((each, index) => {
+  //     return <TweetBoxDisplay key={index}>{each.text}</TweetBoxDisplay>
+  //   })
+  //   setSearchResult(searchResultArray)
+  // }
+
   return (
     <>
-      <h1 className="title">Popular Twitter Figures</h1>
-      <div>
-        <Button className="btn">
-          <img
-            id="button"
-            className="rounded-circle shadow-4-strong"
+      <h1 className="title">Popular Twitter Accounts</h1>
 
-            // src="./assets/martha.jpeg"
-          />
-        </Button>
+      <div className="twitter-icons">
+        <img
+          id="martha"
+          className="rounded-circle shadow-4-strong"
+          src={martha}
+          alt="martha"
+        />
+        <img
+          id="forbes"
+          className="rounded-circle shadow-4-strong"
+          src={forbes}
+          alt="forbes"
+        />{' '}
+        <img
+          id="ramsay"
+          className="rounded-circle shadow-4-strong"
+          src={ramsay}
+          alt="ramsay"
+        />{' '}
+        <img
+          id="natgeo"
+          className="rounded-circle shadow-4-strong"
+          src={natgeo}
+          alt="natgeo"
+        />
       </div>
+
       <div id="random-tweet-display">{randomTweet}</div>
+      <div></div>
     </>
   )
 }
