6f9da95a1767451b1ae7d29f02da12ebaffa07ce
Kurtis <kcasperson7@gmail.com>
2023-05-28 16:17:57 -0700

    Add express request method

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -1,14 +1,30 @@
-import express from 'express';
-import { Express, Request, Response} from 'express';
-const app: Express = express();
+import express from 'express'
+import { Express, Request, Response } from 'express'
+import axios from 'axios'
+
+const app: Express = express()
 const PORT: number = 4321
 
-app.get('https://api.twitter.com/2/tweets/search/recent/?query=hello',( req: Request,res: Response) => {
-    res.json()
-    res.send('typescript file working')
-    
-});
+app.get('/tweet/data/:searchInput', async (req: Request, res: Response) => {
+  try {
+    const { searchInput } = req.params
+    const Response = await axios.get(
+      `https://api.twitter.com/2/tweets/search/recent/?query=${searchInput}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_bearer_token}`,
+        },
+      }
+    )
+    const data = Response.data
+    res.json(data)
+    res.send(JSON.stringify(data))
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
 
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-});
+  console.log(`listening on ${PORT}`)
+})
