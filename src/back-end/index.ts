ccb6d40d7cd7e74967d381dfb44825e1440b2464
Kurtis <kcasperson7@gmail.com>
2023-05-29 10:47:12 -0700

    Add cors Access-Control-Allow-Origin header

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -5,20 +5,32 @@ import axios from 'axios'
 const app: Express = express()
 const PORT: number = 4321
 
+const cors = require('cors')
+
+app.use(cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }))
+
 app.get('/tweet/data/:searchInput', async (req: Request, res: Response) => {
   try {
     const { searchInput } = req.params
-    const Response = await axios.get(
+    const response = await axios.get(
       `https://api.twitter.com/2/tweets/search/recent/?query=${searchInput}`,
+
       {
         headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_bearer_token}`,
+          Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAPly9QAAAAAAOgF9A%2Ff1SJN0O0utkX%2BNF%2B41TkM%3D5He51JIFSZ0dt3Do4oJM3dG7qu3XJ3Lqqych9p6olrbt016GTi`,
         },
       }
     )
-    const data = Response.data
+    // const resp = await axios({
+    //   method: 'GET',
+    //   url: `https://api.twitter.com/2/tweets/search/recent/?query=${searchInput}`,
+    //   headers: {
+    //     Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAPly9QAAAAAAOgF9A%2Ff1SJN0O0utkX%2BNF%2B41TkM%3D5He51JIFSZ0dt3Do4oJM3dG7qu3XJ3Lqqych9p6olrbt016GTi`,
+    //   },
+    // })
+    const data = response.data.data
     res.json(data)
-    res.send(JSON.stringify(data))
+    res.send(JSON.stringify(data.text))
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal server error' })
