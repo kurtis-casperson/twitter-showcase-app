ce3377a21ee7ef227cd28203f19ea9e41f31005c
Kurtis <kcasperson7@gmail.com>
2023-05-29 19:34:42 -0700

    Add dotenv

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 const app: Express = express()
 const PORT: number = 4321
-
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }))
 
@@ -17,17 +17,11 @@ app.get('/tweet/data/:searchInput', async (req: Request, res: Response) => {
 
       {
         headers: {
-          Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAPly9QAAAAAAOgF9A%2Ff1SJN0O0utkX%2BNF%2B41TkM%3D5He51JIFSZ0dt3Do4oJM3dG7qu3XJ3Lqqych9p6olrbt016GTi`,
+          Authorization: `Bearer ${process.env.REACT_APP_bearer_token}`,
         },
       }
     )
-    // const resp = await axios({
-    //   method: 'GET',
-    //   url: `https://api.twitter.com/2/tweets/search/recent/?query=${searchInput}`,
-    //   headers: {
-    //     Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAPly9QAAAAAAOgF9A%2Ff1SJN0O0utkX%2BNF%2B41TkM%3D5He51JIFSZ0dt3Do4oJM3dG7qu3XJ3Lqqych9p6olrbt016GTi`,
-    //   },
-    // })
+
     const data = response.data.data
     res.json(data)
     res.send(JSON.stringify(data.text))
