c373e3a6e42ada190e86a4eb3b683eb25e6767ab
Kurtis <kcasperson7@gmail.com>
2023-06-03 16:26:35 -0700

    Add public metrics as query param

--- src/back-end/index.ts
+++ src/back-end/index.ts
@@ -13,7 +13,7 @@ app.get('/twitter/data/:searchInput', async (req: Request, res: Response) => {
   try {
     const { searchInput } = req.params
     const response = await axios.get(
-      `https://api.twitter.com/2/tweets/search/recent/?query=${searchInput}`,
+      `https://api.twitter.com/2/tweets/search/recent/?query=${searchInput}&tweet.fields=public_metrics`,
 
       {
         headers: {
@@ -24,7 +24,6 @@ app.get('/twitter/data/:searchInput', async (req: Request, res: Response) => {
 
     const data = response.data.data
     res.json(data)
-    res.send(JSON.stringify(data.text))
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal server error' })
@@ -35,7 +34,7 @@ app.get('/twitter/accounts/:id', async (req: Request, res: Response) => {
   try {
     const { id } = req.params
     const response = await axios.get(
-      `https://api.twitter.com/2/users/${id}/tweets`,
+      `https://api.twitter.com/2/users/${id}/tweets?tweet.fields=public_metrics`,
 
       {
         headers: {
@@ -46,26 +45,8 @@ app.get('/twitter/accounts/:id', async (req: Request, res: Response) => {
 
     const data = response.data
     res.json(data)
-    // res.send(JSON.stringify(data))
-    console.log(data.data)
-    console.log('data.data')
 
-    for (const id in data.data) {
-      const secondRes = await axios.get(
-        `https://api.twitter.com/2/tweets?ids=${id}&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_bearer_token}`,
-          },
-        }
-      )
-
-      const tweetData = secondRes.data
-      // res.json(tweetData)
-      // res.send(JSON.stringify(data))
-      console.log(tweetData.data)
-      console.log('tweetData.data')
-    }
+    console.log('data.data', data.data)
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal server error' })
